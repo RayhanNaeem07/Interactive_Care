c5c0b3d7c39337ca8c5c3eda833d1841ee28215a
Rayhan Naeem <a.r.naeem786@gmail.com>
2025-02-26 12:19:41 +0600

    Add:Delete function added

--- noteTakingApp/src/App.jsx
+++ noteTakingApp/src/App.jsx
@@ -3,49 +3,55 @@ import "./App.css";
 
 function App() {
   const [noteTitle, setNoteTitle] = useState("");
-  const [notes, setNotes] = useState([{ Id: 1, title: "Note 1" }]);
-  const [editMode, setEditMode] = useState(false);
-  const [editableNote, setEditableNote] = useState(null);
+  const [notes, setNotes] = useState([{ id: 1, title: "Note 1" }]);
+  const [editMode,setEditMode] = useState(false);
+  const [editableNote,setEditableNote] =useState(null);
 
   const changeTitleHandler = (e) => {
     setNoteTitle(e.target.value);
   };
-  
-  const submitHandler = (e) =>{
+
+  const submitHandler = (e) => {
     e.preventDefault();
 
-    if(noteTitle.trim() === ''){
-      return alert(`Please provide a valid title`);
-    }else{
-      
+    if (noteTitle.trim() === "") {
+      return alert("Please provide a Title");
     }
 
     const newNote = {
       id: Date.now() + "",
-      title: noteTitle
+      title: noteTitle,
     };
 
-    setNotes([...notes,newNote]);
+    setNotes([...notes, newNote]);
     setNoteTitle("");
+
   }
 
+  const removeHandler = (noteId) =>{
+    const updateNotes = notes.filter((item)=> item.id !== noteId);
 
+    setNotes(updateNotes);
+  }
+
+  
 
   return (
     <div className="App">
       <form onSubmit={submitHandler}>
         <input type="text" value={noteTitle} onChange={changeTitleHandler} />
-        <button type="submit">Add Note</button>
+        <button>Add Note</button>
       </form>
+
       <div className="note-list">
-        <h2>All notes</h2>
+        <h2>All Notes</h2>
         <ul>
           {notes.map((note) => (
             <>
               <li key={note.id}>
                 <span>{note.title}</span>
-                <button>Edit</button>
-                <button>Delete</button>
+                <button >Edit</button>
+                <button onClick={()=>removeHandler(note.id)}>Delete</button>
               </li>
               <br />
             </>
