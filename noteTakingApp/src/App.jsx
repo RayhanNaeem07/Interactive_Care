bbd8c0077a0f6ac82cb7ca39a7210f9bf6184764
Rayhan Naeem <a.r.naeem786@gmail.com>
2025-02-26 19:34:00 +0600

    Add:Edit and update Feature added

--- noteTakingApp/src/App.jsx
+++ noteTakingApp/src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 
 function App() {
   const [noteTitle, setNoteTitle] = useState("");
-  const [notes, setNotes] = useState([{ id: 1, title: "Note 1" }]);
-  const [editMode,setEditMode] = useState(false);
-  const [editableNote,setEditableNote] =useState(null);
+  const [notes, setNotes] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editableNote, setEditableNote] = useState(null);
 
   const changeTitleHandler = (e) => {
     setNoteTitle(e.target.value);
@@ -17,7 +17,10 @@ function App() {
     if (noteTitle.trim() === "") {
       return alert("Please provide a Title");
     }
+    editMode ? updateHandler() : createHandler();
+  };
 
+  const createHandler = () => {
     const newNote = {
       id: Date.now() + "",
       title: noteTitle,
@@ -25,22 +28,37 @@ function App() {
 
     setNotes([...notes, newNote]);
     setNoteTitle("");
+  };
 
-  }
-
-  const removeHandler = (noteId) =>{
-    const updateNotes = notes.filter((item)=> item.id !== noteId);
+  const removeHandler = (noteId) => {
+    const updateNotes = notes.filter((item) => item.id !== noteId);
 
     setNotes(updateNotes);
-  }
+  };
 
-  
+  const editHandler = (note) => {
+    setEditMode(true);
+    setEditableNote(note);
+    setNoteTitle(note.title);
+  };
+
+  const updateHandler = () => {
+    const updatedNotes = notes.map(item =>{
+      if(item.id === editableNote.id){
+        return {...item, title: noteTitle};
+      }
+      return item;
+    });
+    setNotes(updatedNotes);
+    setEditMode(false);
+    setNoteTitle("");
+  };
 
   return (
     <div className="App">
       <form onSubmit={submitHandler}>
         <input type="text" value={noteTitle} onChange={changeTitleHandler} />
-        <button>Add Note</button>
+        <button type="submit">{editMode ? "Update Note" : "Add Note"}</button>
       </form>
 
       <div className="note-list">
@@ -50,8 +68,8 @@ function App() {
             <>
               <li key={note.id}>
                 <span>{note.title}</span>
-                <button >Edit</button>
-                <button onClick={()=>removeHandler(note.id)}>Delete</button>
+                <button onClick={() => editHandler(note)}>Edit</button>
+                <button onClick={() => removeHandler(note.id)}>Delete</button>
               </li>
               <br />
             </>
